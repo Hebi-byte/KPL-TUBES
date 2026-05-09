cdfcc6427a45b334cf00fe382c71359ede309470
Hebi <hary24036@gmail.com>
2026-05-09 22:19:49 +0700

    fixing

--- src/Routes/statusRoutes.js
+++ src/Routes/statusRoutes.js
@@ -4,15 +4,10 @@ const router = express.Router();
 const {
   getAllStatuses,
   getStatusById,
-  createStatus,
-  updateStatus,
-  deleteStatus,
 } = require("../controllers/statusController");
 
 router.get("/", getAllStatuses);
 router.get("/:id", getStatusById);
 router.post("/", createStatus);
-router.put("/:id", updateStatus);
-router.delete("/:id", deleteStatus);
 
 module.exports = router;
--- src/Routes/workflowRoutes.js
+++ src/Routes/workflowRoutes.js
@@ -4,15 +4,10 @@ const router = express.Router();
 const {
   getAllWorkflow,
   getWorkflowById,
-  createWorkflow,
-  updateWorkflow,
-  deleteWorkflow,
 } = require("../controllers/workflowController");
 
 router.get("/", getAllWorkflow);
 router.get("/:id", getWorkflowById);
 router.post("/", createWorkflow);
-router.put("/:id", updateWorkflow);
-router.delete("/:id", deleteWorkflow);
 
 module.exports = router;
