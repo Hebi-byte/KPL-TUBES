9da52380c4a4fd741b90dcd4a58dacf467225795
danuwarisman <danuwarisman514@gmail.com>
2026-06-16 00:57:32 +0700

    wire logger & errorHandler di app.js

--- src/app.js
+++ src/app.js
@@ -2,6 +2,11 @@ const express = require("express");
 const path = require("path");
 require("dotenv").config();
 
+const app = express();
+
+const logger = require('./middleware/logger');
+const errorHandler = require('./middleware/errorHandler');
+
 const userRoutes = require("./Routes/userRoutes");
 const projectRoutes = require("./Routes/projectRoutes");
 const taskRoutes = require("./Routes/taskRoutes");
@@ -10,11 +15,12 @@ const rolesRoutes = require("./Routes/rolesRoutes");
 const workflowRoutes = require("./Routes/workflowRoutes");
 const authRoutes = require("./Routes/authRoutes");
 
-const app = express();
 const publicPath = path.join(__dirname, "../Public");
 
 app.use(express.json());
 app.use(express.static(publicPath));
+// Logger masuk sebelum routes, biar semua request tercatat
+app.use(logger);
 
 app.use("/api/users", userRoutes);
 app.use("/api/projects", projectRoutes);
@@ -39,6 +45,8 @@ app.get(["/dashboard", "/projects/:id"], (req, res) => {
 app.get(/^\/(?!api|test-db|test-env|login|dashboard|projects).*/, (req, res) => {
   res.redirect("/login");
 });
+// Error handler masuk setelah semua routes, biar bisa tangkap error dari mana saja
+app.use(errorHandler);
 
 const PORT = process.env.PORT || 3000;
 
